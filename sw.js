62d591fe220fa2eb60dcad523d06ac6e6b77d052
karwan <lkarwan95@gmail.com>
2026-08-24 19:22:49 +0800

    Publish site (2026-08-24 11:22)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //
 // Strategy: serve from cache first (fast, works offline), fetch a fresh copy in
 // the background. The next open shows the new version.
-const CACHE = "baobaoxiang-vf7083911";
+const CACHE = "baobaoxiang-v7cbad56d";
 const FILES = ["./", "./index.html", "./manifest.json",
                "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 
